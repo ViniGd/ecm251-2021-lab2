98f3f5e6d47fdba872cc0a981f16e883adadaee4
ViniGd <79457724+ViniGd@users.noreply.github.com>
2021-05-02 11:39:17 -0300

    Arrumando o codigo

--- aula 02/src/godoy/abe/vinicius/Main.java
+++
@@ -1,8 +0,0 @@
-package godoy.abe.vinicius;
-
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-    }
-}
---
+++ aula 02/src/godoy/abe/vinicius/OlaMundo.java
@@ -0,0 +1,12 @@
+package godoy.abe.vinicius;
+
+public class OlaMundo {
+
+    public static void main(String[] args) {
+	   // Comentario de uma linha
+	System.out.println("Ola Mundo!");
+	   /* 
+	   * Comentario de varias linhas
+	   *\
+    }
+}
